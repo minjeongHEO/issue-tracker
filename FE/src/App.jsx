1da984ae2dd43e060c95ab41b1dfbb620acf1600
minjeongHEO <hmjoung9770@gmail.com>
2024-05-08 16:54:14 +0900

    feat: body css 적용 및 style 테마 생성

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
-import "./css/App.css";
-import { AppRoutes } from "./router/routes";
+import { useEffect, useState } from 'react';
+import { AppRoutes } from './router/routes';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './styles/theme.js';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const userTheme = useState('lightTheme');
+    useEffect(() => {}, [userTheme]);
 
     return (
-        <div>
-            <AppRoutes />
-        </div>
+        <ThemeProvider theme={lightTheme}>
+            <Root>
+                <AppRoutes />
+            </Root>
+        </ThemeProvider>
     );
 }
+
+const Root = styled.div`
+    background-color: ${(props) => props.theme.bgColorBody};
+    color: ${(props) => props.theme.fontColor};
+    place-items: center;
+    display: flex;
+    flex-direction: column; /* 자식 요소들을 세로 방향으로 정렬 */
+    justify-content: center; /* 세로 방향으로 중앙 정렬 */
+    align-items: center; /* 가로 방향으로 중앙 정렬 */
+    height: 100vh;
+    width: 100vw;
+    margin: 0 auto;
+    text-align: center;
+`;
 
 export default App;
---
+++ FE/src/styles/theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+    bgColorBody: '#FFF',
+    fontColor: '#363537',
+    dropshadowColor: '#14142B, 4%, Blur 8',
+};
+
+export const darkTheme = {
+    bgColorBody: '#363537',
+    fontColor: '#FAFAFA',
+    dropshadowColor: '#14142B, 80%, Blur 16',
+};
