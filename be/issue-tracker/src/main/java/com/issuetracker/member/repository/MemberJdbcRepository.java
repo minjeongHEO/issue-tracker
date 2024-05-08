750e3e4c1258243fdad3f6253990164ea204244b
sangchu <didi1484@gmail.com>
2024-05-08 15:33:11 +0900

    feat: 레포지토리 기능 구현
    - 스프링 데이터 JDBC 사용하여 레포지토리 구현하였음

--- be/issue-tracker/src/main/java/com/issuetracker/member/repository/MemberJdbcRepository.java
+++ be/issue-tracker/src/main/java/com/issuetracker/member/repository/MemberJdbcRepository.java
@@ -1,68 +1,118 @@
 package com.issuetracker.member.repository;
 
 import com.issuetracker.member.model.Member;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberJdbcRepository implements MemberRepository {
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public <S extends Member> S save(S entity) {
-        return null;
+        String sql = "INSERT INTO member (id, password, nickname, email) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, entity.getId(), entity.getPassword(), entity.getNickname(), entity.getEmail());
+        return entity;
     }
 
     @Override
     public <S extends Member> Iterable<S> saveAll(Iterable<S> entities) {
-        return null;
+        for (S entity : entities) {
+            save(entity);
+        }
+        return entities;
     }
 
     @Override
-    public Optional<Member> findById(String s) {
-        return Optional.empty();
+    public Optional<Member> findById(String id) {
+        String sql = "SELECT id, password, nickname, email FROM member WHERE id = ?";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Member.class), id)
+                .stream()
+                .findAny();
     }
 
     @Override
-    public boolean existsById(String s) {
-        return false;
+    public boolean existsById(String id) {
+        String sql = "SELECT COUNT(*) FROM member WHERE id = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, id);
+        return count != null && count > 0;
     }
 
     @Override
     public Iterable<Member> findAll() {
-        return null;
+        String sql = "SELECT id, password, nickname, email FROM member";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Member.class));
     }
 
     @Override
-    public Iterable<Member> findAllById(Iterable<String> strings) {
-        return null;
+    public Iterable<Member> findAllById(Iterable<String> ids) {
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
+
+        String sql = "SELECT id, password, nickname, email FROM member WHERE id IN (:ids)";
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("ids", ids);
+
+        return namedParameterJdbcTemplate.query(sql, parameters, new BeanPropertyRowMapper<>(Member.class));
     }
 
     @Override
     public long count() {
-        return 0;
+        String sql = "SELECT COUNT(*) from member";
+        return jdbcTemplate.queryForObject(sql, Long.class);
     }
 
     @Override
-    public void deleteById(String s) {
-
+    public void deleteById(String id) {
+        String sql = "DELETE FROM member WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 
     @Override
-    public void delete(Member entity) {
-
+    public void delete(Member member) {
+        deleteById(member.getId());
     }
 
     @Override
-    public void deleteAllById(Iterable<? extends String> strings) {
+    public void deleteAllById(Iterable<? extends String> ids) {
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("ids", ids);
 
+        String sql = "DELETE FROM member WHERE id IN (:ids)";
+        namedParameterJdbcTemplate.update(sql, mapSqlParameterSource);
     }
 
     @Override
     public void deleteAll(Iterable<? extends Member> entities) {
+        List<String> ids = getIds(entities);
 
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("ids", ids);
+
+        String sql = "DELETE FROM member WHERE id IN (:ids)";
+        namedParameterJdbcTemplate.update(sql, mapSqlParameterSource);
     }
 
     @Override
     public void deleteAll() {
+        String sql = "DELETE FROM member";
+        jdbcTemplate.update(sql);
+    }
 
+    private List<String> getIds(Iterable<? extends Member> entities) {
+        List<String> ids = new ArrayList<>();
+        for (Member entity : entities) {
+            ids.add(entity.getId());
+        }
+        return ids;
     }
 }
