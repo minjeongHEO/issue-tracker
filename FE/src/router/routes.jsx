ad32e9586de7f7a6c5f38f87b5bf6d8b9e08ded3
minjeongHEO <hmjoung9770@gmail.com>
2024-05-08 15:39:03 +0900

    feat: 404페이지 라우터 생성

--- FE/src/router/routes.jsx
+++ FE/src/router/routes.jsx
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Join from "../components/Join";
-import Login from "../components/Login";
-import Main from "../components/Main";
-import ProtectedRoute from "./ProtectedRoute";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Join from '../components/Join';
+import Login from '../components/Login';
+import Main from '../components/Main';
+import NotFound from '../components/NotFound';
+import ProtectedRoute from './ProtectedRoute';
 
 export const AppRoutes = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/join" element={<Join />} />
-                <Route
-                    path="/"
-                    element={
-                        <ProtectedRoute>
-                            <Main />
-                        </ProtectedRoute>
-                    }
-                />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
