383dec37bd97083e0c2ae4b1ca6b02a3921620c1
zzawang <hobin0116@naver.com>
2024-05-09 15:55:54 +0900

    feat: LabelController에 새 라벨 생성하는 API 구현
    - 새 라벨을 생성한다.
    - 라벨의 이름을 넣은 API를 URI 객체로 만들어서 헤더에 넣고 라벨을 바디에 넣어 상태코드 201로 전달한다.

---
+++ be/issue-tracker/src/main/java/com/issuetracker/label/controller/LabelController.java
@@ -0,0 +1,27 @@
+package com.issuetracker.label.controller;
+
+import com.issuetracker.label.domain.Label;
+import com.issuetracker.label.dto.LabelDto;
+import com.issuetracker.label.service.LabelService;
+import jakarta.validation.Valid;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/labels")
+@RequiredArgsConstructor
+public class LabelController {
+    private final LabelService labelService;
+
+    @PostMapping
+    public ResponseEntity<Label> createLabels(@Valid @RequestBody LabelDto labelDto) {
+        Label label = labelService.createNewLabel(labelDto);
+        URI location = URI.create(String.format("/api/labels/%s", label.getName()));
+        return ResponseEntity.created(location).body(label);
+    }
+}
