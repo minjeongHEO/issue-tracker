426ff0c99786e4238da5b0055b494c635cb77a94
minjeongHEO <hmjoung9770@gmail.com>
2024-05-10 16:23:25 +0900

    fix: JSON세션 스토리지 저장 오류

--- FE/src/components/Login.jsx
+++ FE/src/components/Login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
 
             if (loginResult.result === 'ok') {
                 setLoginCheck(true);
-                sessionStorage.setItem('storeUserData', loginResult.data);
+                sessionStorage.setItem('storeUserData', JSON.stringify(loginResult.data));
                 navigate('/');
             }
         } catch (error) {
