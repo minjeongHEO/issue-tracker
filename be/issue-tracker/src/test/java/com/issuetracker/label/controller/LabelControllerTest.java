456ab1f0bfad949b19412f6c271fd5410e160e9f
zzawang <hobin0116@naver.com>
2024-05-10 23:24:51 +0900

    test: 라벨 수정 API 실패 테스트 추가
    - 라벨 수정 API에서 데이터 바인딩이 실패한 경우 상태코드 400을 반환한다.
    - 라벨 수정 API에서 라벨 DTO의 배경 색깔이 유효하지 않다면 상태코드 400을 반환한다.
    - 라벨 수정 API로 존재하지 않는 라벨 아이디를 수정 요청하면 수정하지 않고 상태코드 404를 반환한다.

--- be/issue-tracker/src/test/java/com/issuetracker/label/controller/LabelControllerTest.java
+++ be/issue-tracker/src/test/java/com/issuetracker/label/controller/LabelControllerTest.java
@@ -1,6 +1,7 @@
 package com.issuetracker.label.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -27,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -126,6 +128,44 @@ class LabelControllerTest {
                 .andExpect(jsonPath("$.name").value(updatedLabel.getName()))
                 .andExpect(jsonPath("$.description").value(updatedLabel.getDescription()))
                 .andExpect(jsonPath("$.bgColor").value(updatedLabel.getBgColor()));
+    }
+
+    @DisplayName("라벨 수정 API에서 데이터 바인딩이 실패한 경우 상태코드 400을 반환한다.")
+    @Test
+    public void modifyLabels_WithBindingErrors() throws Exception {
+        // 예를 들어, Name 필드가 비어있어서 유효하지 않은 경우로 설정
+        labelDto.setName("");
+
+        mockMvc.perform(put("/api/labels/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(labelDto)))
+                .andExpect(status().isBadRequest()); // BAD_REQUEST(400) 상태 코드를 기대한다.
+    }
+
+    @DisplayName("라벨 수정 API에서 라벨 DTO의 배경 색깔이 유효하지 않다면 상태코드 400을 반환한다.")
+    @Test
+    public void modifyLabels_WithInvalidBgColor() throws Exception {
+        // 유효하지 않은 색상 코드로 배경 색 설정
+        labelDto.setBgColor("#12"); // 유효하지 않은 색상 코드
+
+        given(labelService.modifyLabel(any(LabelDto.class), anyLong())).willThrow(InvalidBgColorException.class);
+
+        mockMvc.perform(put("/api/labels/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(labelDto)))
+                .andExpect(status().isBadRequest()); // BAD_REQUEST(400) 상태 코드를 기대한다.
+    }
+
+    @DisplayName("라벨 수정 API로 존재하지 않는 라벨 아이디를 수정 요청하면 수정하지 않고 상태코드 404를 반환한다.")
+    @Test
+    public void modifyLabels_WithNonExistedName() throws Exception {
+        given(labelService.modifyLabel(any(LabelDto.class), anyLong()))
+                .willThrow(IncorrectUpdateSemanticsDataAccessException.class);
+
+        mockMvc.perform(put("/api/labels/{id}", 10000L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(labelDto)))
+                .andExpect(status().isNotFound()); // NOT FOUND(404) 상태 코드를 기대한다.
     }
 
     @DisplayName("라벨 삭제 API를 사용하여 라벨을 삭제할 수 있다.")
