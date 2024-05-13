2506c5c0bff6d7956cc54cca9a7ba6a27f42a254
zzawang <hobin0116@naver.com>
2024-05-14 00:14:40 +0900

    feat: isClosed 여부에 따라 메인 화면에 보여질 이슈 리스트 생성 기능 구현
    - isClosed 여부에 따라 조회된 이슈와 그와 연관된 라벨, 마일스톤을 함께 담은 DTO를 반환한다.

---
+++ be/issue-tracker/src/main/java/com/issuetracker/issue/service/IssueService.java
@@ -0,0 +1,63 @@
+package com.issuetracker.issue.service;
+
+import com.issuetracker.issue.domain.Issue;
+import com.issuetracker.issue.dto.IssueListDto;
+import com.issuetracker.issue.dto.IssueListDto.IssueListDtoBuilder;
+import com.issuetracker.issue.repository.IssueLabelRepository;
+import com.issuetracker.issue.repository.IssueRepository;
+import com.issuetracker.label.dto.LabelCoverDto;
+import com.issuetracker.label.repository.LabelRepository;
+import com.issuetracker.milestone.Repository.MilestoneRepository;
+import com.issuetracker.milestone.domain.Milestone;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class IssueService {
+    private final IssueRepository issueRepository;
+    private final IssueLabelRepository issueLabelRepository;
+    private final MilestoneRepository milestoneRepository;
+    private final LabelRepository labelRepository;
+
+    @Transactional(readOnly = true)
+    public List<IssueListDto> getIssuesByIsClosed(boolean isClosed) {
+        List<Issue> selectedIssues = issueRepository.findAllByIsClosed(isClosed);
+        return selectedIssues.stream()
+                .map(this::toIssueListDto)
+                .collect(Collectors.toList());
+    }
+
+    private IssueListDto toIssueListDto(Issue issue) {
+        IssueListDtoBuilder builder = IssueListDto.builder()
+                .id(issue.getId())
+                .title(issue.getTitle())
+                .memberId(issue.getMemberId())
+                .createDate(issue.getCreateDate());
+
+        addLabels(builder, issue.getId());
+        addMilestone(builder, issue.getMilestoneId());
+        return builder.build();
+    }
+
+    private void addLabels(IssueListDtoBuilder builder, Long issueId) {
+        List<Long> labelIds = issueLabelRepository.findAllByIssueId(issueId);
+        if (!labelIds.isEmpty()) {
+            List<LabelCoverDto> labelCovers = labelRepository.findLabelCoverDtoById(labelIds);
+            builder.labels(labelCovers);
+        }
+    }
+
+    private void addMilestone(IssueListDtoBuilder builder, Long milestoneId) {
+        if (milestoneId != null) {
+            milestoneRepository.findById(milestoneId)
+                    .map(Milestone::getName)
+                    .ifPresent(builder::milestoneName);
+        }
+    }
+}
