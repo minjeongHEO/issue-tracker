21681fb0389a81cee4d7c18124b580edf9541032
minjeongHEO <hmjoung9770@gmail.com>
2024-05-13 16:33:05 +0900

    feat: 라벨, 마일스톤 라우팅 지정

--- FE/src/router/routes.jsx
+++ FE/src/router/routes.jsx
@@ -3,6 +3,8 @@ import Join from '../components/Join';
 import Login from '../components/Login';
 import Index from '../components/Index';
 import NotFound from '../components/NotFound';
+import Milestones from '../components/Milestones';
+import Labels from '../components/Labels';
 import ProtectedRoute from './ProtectedRoute';
 
 export const AppRoutes = () => {
@@ -11,6 +13,8 @@ export const AppRoutes = () => {
             <Routes>
                 <Route path="/members/login" element={<Login />} />
                 <Route path="/members/join" element={<Join />} />
+                <Route path="/milestones" element={<Milestones />} />
+                <Route path="/labels" element={<Labels />} />
                 <Route
                     path="/"
                     element={
