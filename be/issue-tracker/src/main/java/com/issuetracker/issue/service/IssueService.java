4af44d7fe4228fd2a8d79be094f1016b18c47780
zzawang <hobin0116@naver.com>
2024-05-16 20:22:21 +0900

    feat: IssueService에 연관관계에 있는 Repository 탐색 기능 구현
    - 특정 이슈에 있는 라벨들의 아이디 리스트를 반환한다.
    - 특정 이슈를 담당하는 담당자들의 아이디 리스트를 반환한다.

--- be/issue-tracker/src/main/java/com/issuetracker/issue/service/IssueService.java
+++ be/issue-tracker/src/main/java/com/issuetracker/issue/service/IssueService.java
@@ -1,17 +1,10 @@
 package com.issuetracker.issue.service;
 
-import com.issuetracker.issue.domain.Issue;
 import com.issuetracker.issue.dto.IssueCountDto;
-import com.issuetracker.issue.dto.IssueListDto;
-import com.issuetracker.issue.dto.IssueListDto.IssueListDtoBuilder;
+import com.issuetracker.issue.repository.IssueAssigneeRepository;
 import com.issuetracker.issue.repository.IssueLabelRepository;
 import com.issuetracker.issue.repository.IssueRepository;
-import com.issuetracker.label.dto.LabelCoverDto;
-import com.issuetracker.label.repository.LabelRepository;
-import com.issuetracker.milestone.Repository.MilestoneRepository;
-import com.issuetracker.milestone.domain.Milestone;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,18 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class IssueService {
     private final IssueRepository issueRepository;
+    private final IssueAssigneeRepository issueAssigneeRepository;
     private final IssueLabelRepository issueLabelRepository;
-    private final MilestoneRepository milestoneRepository;
-    private final LabelRepository labelRepository;
 
-    @Transactional(readOnly = true)
-    public List<IssueListDto> getIssuesByIsClosed(boolean isClosed) {
-        List<Issue> selectedIssues = issueRepository.findAllByIsClosed(isClosed);
-        return selectedIssues.stream()
-                .map(this::toIssueListDto)
-                .collect(Collectors.toList());
-    }
-
+    /**
+     * 열린 이슈의 개수와 닫힌 이슈의 개수를 구한다.
+     */
     @Transactional(readOnly = true)
     public IssueCountDto getIssueCountDto() {
         long openedIssueCount = issueRepository.countAllByIsClosed(false);
@@ -42,31 +29,19 @@ public class IssueService {
         return new IssueCountDto(openedIssueCount, closedIssueCount);
     }
 
-    private IssueListDto toIssueListDto(Issue issue) {
-        IssueListDtoBuilder builder = IssueListDto.builder()
-                .id(issue.getId())
-                .title(issue.getTitle())
-                .memberId(issue.getMemberId())
-                .createDate(issue.getCreateDate());
-
-        addLabels(builder, issue.getId());
-        addMilestone(builder, issue.getMilestoneId());
-        return builder.build();
+    /**
+     * 특정 이슈에 있는 라벨들의 아이디 리스트를 반환한다.
+     */
+    @Transactional(readOnly = true)
+    public List<Long> findLabelIdsByIssueId(Long issueId) {
+        return issueLabelRepository.findLabelIdsByIssueId(issueId);
     }
 
-    private void addLabels(IssueListDtoBuilder builder, Long issueId) {
-        List<Long> labelIds = issueLabelRepository.findAllByIssueId(issueId);
-        if (!labelIds.isEmpty()) {
-            List<LabelCoverDto> labelCovers = labelRepository.findLabelCoverDtoById(labelIds);
-            builder.labels(labelCovers);
-        }
-    }
-
-    private void addMilestone(IssueListDtoBuilder builder, Long milestoneId) {
-        if (milestoneId != null) {
-            milestoneRepository.findById(milestoneId)
-                    .map(Milestone::getName)
-                    .ifPresent(builder::milestoneName);
-        }
+    /**
+     * 특정 이슈를 담당하는 담당자들의 아이디 리스트를 반환한다.
+     */
+    @Transactional(readOnly = true)
+    public List<String> findAssigneeIdsByIssueId(Long issueId) {
+        return issueAssigneeRepository.findAssigneeIdsByIssueId(issueId);
     }
 }
