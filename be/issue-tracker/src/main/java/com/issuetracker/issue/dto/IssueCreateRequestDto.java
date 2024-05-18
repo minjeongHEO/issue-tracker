ae4ac66314104ae1341bfd47b2ad776139483aa7
sangchu <didi1484@gmail.com>
2024-05-18 18:13:43 +0900

    feat: 이슈 라벨 수정 기능 구현

--- be/issue-tracker/src/main/java/com/issuetracker/issue/dto/IssueCreateRequestDto.java
+++ be/issue-tracker/src/main/java/com/issuetracker/issue/dto/IssueCreateRequestDto.java
@@ -1,7 +1,6 @@
 package com.issuetracker.issue.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,8 +18,6 @@ public class IssueCreateRequestDto {
     private final String authorId; // 아직 로그인 유지 기능이 없으므로 사용자에게 입력받음
     private final Long milestoneId;
     private final Long fileId;
-    @NotNull
     private final List<Long> labelIds;
-    @NotNull
     private final List<String> assigneeIds;
 }
