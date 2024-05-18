ba831eb207cd47a9bd31cb46a1a59810d3d080dc
sangchu <didi1484@gmail.com>
2024-05-18 17:04:07 +0900

    feat: 이슈 제목 수정 기능 구현

--- be/issue-tracker/src/main/java/com/issuetracker/issue/repository/IssueRepository.java
+++ be/issue-tracker/src/main/java/com/issuetracker/issue/repository/IssueRepository.java
@@ -1,6 +1,8 @@
 package com.issuetracker.issue.repository;
 
 import com.issuetracker.issue.domain.Issue;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IssueRepository extends CrudRepository<Issue, Long> {
@@ -8,4 +10,8 @@ public interface IssueRepository extends CrudRepository<Issue, Long> {
     Long countByMilestoneIdAndIsClosed(Long milestoneId, boolean isClosed);
 
     long countAllByIsClosed(boolean isClosed);
+
+    @Modifying
+    @Query("UPDATE issue SET title = :title WHERE id = :id")
+    void updateTitleById(Long id, String title);
 }
