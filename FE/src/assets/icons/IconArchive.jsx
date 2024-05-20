94d11d4c1ab9455543a4a0c16eb9b3693c5249e2
minjeongHEO <hmjoung9770@gmail.com>
2024-05-20 20:21:38 +0900

    fix: jsx 속성 문법으로 변경

--- FE/src/assets/icons/IconArchive.jsx
+++ FE/src/assets/icons/IconArchive.jsx
@@ -4,16 +4,16 @@ export function IconArchive({ className, children }) {
     return (
         <StyledWrapper className={className}>
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_14852_5722)">
-                    <path d="M14 5.33337V14H2V5.33337" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                <g clipPath="url(#clip0_14852_5722)">
+                    <path d="M14 5.33337V14H2V5.33337" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
                     <path
                         d="M15.3332 2H0.666504V5.33333H15.3332V2Z"
                         stroke="currentColor"
-                        stroke-width="1.6"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.6"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     />
-                    <path d="M6.6665 8H9.33317" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M6.6665 8H9.33317" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
                 </g>
                 <defs>
                     <clipPath id="clip0_14852_5722">
