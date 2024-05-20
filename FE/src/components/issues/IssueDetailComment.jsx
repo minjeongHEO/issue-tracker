4a75f84c4395dfb63ca738841fc8b2a8b6f120fd
minjeongHEO <hmjoung9770@gmail.com>
2024-05-20 20:24:51 +0900

    feat: 이슈 상세 - 코멘트 컴포넌트 생성

---
+++ FE/src/components/issues/IssueDetailComment.jsx
@@ -0,0 +1,90 @@
+import { Button } from 'antd';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { FlexCol, FlexRow } from '../../styles/theme';
+import { CustomProfile } from '../../assets/CustomProfile';
+import { calculatePastTime } from '../../utils/dateUtils';
+
+export default function IssueDetailComment({ detailData }) {
+    if (!detailData) return <div>Loading...</div>;
+
+    const { id, title, content, createDate, writer, milestone, file, labels, assignees, comments, isclosed } = detailData;
+    const [pastTime, setPastTime] = useState('');
+
+    useEffect(() => {
+        setPastTime(calculatePastTime(createDate));
+
+        const intervalPerTime = () => {
+            setInterval(() => {
+                setPastTime(calculatePastTime(createDate));
+            }, 1000 * 60);
+        };
+
+        return () => clearInterval(intervalPerTime);
+    }, [createDate]);
+
+    return (
+        <StyledCommentContainer>
+            <CommentNav>
+                <CommentData>
+                    <StyledProfile
+                        src={'https://github.com/codesquad-masters2024-team02/issue-tracker/assets/96780693/d1c7123b-89c9-485b-b9dd-8cc21a1005e0'}
+                        alt={'userProfile'}
+                        size={'medium'}
+                    />
+                    <span className="userName">woody</span>
+                    {/* <span className="">{calculatePastTime(createDate)}</span> */}
+                    <span className="">{pastTime}</span>
+                </CommentData>
+                <NavBtnContainer>
+                    <div>작성자</div>
+                    <div>편집</div>
+                    <div>반응</div>
+                </NavBtnContainer>
+            </CommentNav>
+        </StyledCommentContainer>
+    );
+}
+
+const StyledProfile = styled(CustomProfile)`
+    margin-left: 10px;
+`;
+const NavBtnContainer = styled(FlexRow)`
+    justify-content: space-around;
+    background-color: red;
+    width: 150px;
+`;
+
+const CommentData = styled(FlexRow)`
+    * {
+        margin-left: 10px;
+    }
+`;
+
+const StyledFlexRow = styled(FlexRow)`
+    & .commentData {
+        /* flex-basis: 70%; */
+    }
+`;
+
+const CommentNav = styled(FlexRow)`
+    width: 100%;
+    height: 50px;
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+    border-bottom: 1px solid ${(props) => props.theme.borderColor};
+    background-color: ${(props) => props.theme.listHeaderColor};
+    color: ${(props) => props.theme.fontColor};
+`;
+const StyledCommentContainer = styled.div`
+    /* background-color: antiquewhite; */
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    min-height: 100px;
+    margin-top: 15px;
+    background-color: ${(props) => props.theme.bgColorBody};
+    border-radius: 10px;
+    border: 1px solid ${(props) => props.theme.borderColor};
+    color: ${(props) => props.theme.fontColor};
+`;
