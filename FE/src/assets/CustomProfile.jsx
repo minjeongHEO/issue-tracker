b4b8624d03adbcfc76c990970145e7e9bf184f8d
minjeongHEO <hmjoung9770@gmail.com>
2024-05-20 20:16:36 +0900

    feat: 프로필 이미지 컴포넌트 생성

---
+++ FE/src/assets/CustomProfile.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import theme from '../styles/theme';
+
+export function CustomProfile({ className, children, src = '', alt = '', size = 'small' }) {
+    return (
+        <StyledImg className={className} src={src} $size={size} alt={alt}>
+            {children}
+        </StyledImg>
+    );
+}
+
+const StyledImg = styled.img`
+    border-radius: 50%;
+    width: ${(props) => theme.imgSize[props.$size]};
+    height: ${(props) => theme.imgSize[props.$size]};
+    border: 1px solid ${(props) => props.theme.borderColor};
+    object-fit: cover;
+`;
