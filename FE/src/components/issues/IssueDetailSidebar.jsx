3b0c1d8ae0c28f97f336995a7cf566162cf628ed
minjeongHEO <hmjoung9770@gmail.com>
2024-05-25 16:46:50 +0900

    feat: 사이드바 필터 컴포넌트로 분리

--- FE/src/components/issues/IssueDetailSidebar.jsx
+++ FE/src/components/issues/IssueDetailSidebar.jsx
@@ -9,6 +9,7 @@ import { IconTrash } from '../../assets/icons/IconTrash';
 import { Popconfirm, message } from 'antd';
 import { useDeleteIssue } from '../../hooks/useIssueDetailData';
 import { Link, useNavigate } from 'react-router-dom';
+import OptionSidebar from './OptionSidebar';
 
 export default function IssueDetailSidebar({ milestone, assignees, labels, issueId }) {
     const navigate = useNavigate();
@@ -18,56 +19,20 @@ export default function IssueDetailSidebar({ milestone, assignees, labels, issue
     };
     const { mutate: deleteIssue } = useDeleteIssue(issueId, onSuccess);
 
-    const openIssueCount = milestone?.openIssueCount ?? 0;
-    const closedIssueCount = milestone?.closedIssueCount ?? 0;
-
-    const calulatePercentage = (openCount, closedCount) => (closedCount / (openCount + closedCount)).toFixed(2);
     const deleteConfirm = () => deleteIssue();
 
     return (
         <StyledDiv>
             <SidebarContainer>
-                <Filter>
-                    <FilterTitle>
-                        <div>담당자</div>
-                        <IconPlus />
-                    </FilterTitle>
-                    {assignees.map(({ id, imgUrl }) => (
-                        <FilterContentContainer key={id}>
-                            <CustomProfile src={imgUrl} alt={'assineeProfile'} />
-                            <span className="userName">{id}</span>
-                        </FilterContentContainer>
-                    ))}
-                </Filter>
-                <StyledLine />
-                <Filter>
-                    <FilterTitle>
-                        <div>레이블</div>
-                        <IconPlus />
-                    </FilterTitle>
-                    <LabelContentContainer>
-                        {labels.map(({ id, name, description, textColor, bgColor }) => (
-                            <StyledLabel key={id} backgroundColor={bgColor} color={textColor}>
-                                {name}
-                            </StyledLabel>
-                        ))}
-                    </LabelContentContainer>
-                </Filter>
-                <StyledLine />
-                <Filter>
-                    <FilterTitle>
-                        <div>마일스톤</div>
-                        <IconPlus />
-                    </FilterTitle>
-                    {milestone && (
-                        <>
-                            <FilterContentContainer>
-                                <IconProgressBar percentage={calulatePercentage(openIssueCount, closedIssueCount)} />
-                            </FilterContentContainer>
-                            <FilterContentContainer>{milestone.name}</FilterContentContainer>
-                        </>
-                    )}
-                </Filter>
+                <OptionSidebar filterName={'assignees'} filterData={assignees}>
+                    담당자
+                </OptionSidebar>
+                <OptionSidebar filterName={'labels'} filterData={labels}>
+                    레이블
+                </OptionSidebar>
+                <OptionSidebar filterName={'milestone'} filterData={milestone}>
+                    마일스톤
+                </OptionSidebar>
             </SidebarContainer>
             <DeleteContentContainer>
                 <Popconfirm title="이슈를 삭제하시겠습니까?" onConfirm={deleteConfirm} okText="Yes" cancelText="No">
@@ -81,24 +46,6 @@ export default function IssueDetailSidebar({ milestone, assignees, labels, issue
     );
 }
 
-const StyledLabel = styled(CustomLabelBadge)`
-    /* height: 30px; */
-    margin-right: 10px;
-    font-size: 13px;
-`;
-
-const StyledLine = styled.div`
-    width: 100%;
-    height: 1px;
-    border-bottom: 1px solid ${(props) => props.theme.borderColor};
-`;
-
-const LabelContentContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    margin: 20px 0;
-`;
-
 const DeleteContentContainer = styled(FlexRow)`
     width: 100%;
     margin: 20px 0 10px 0;
@@ -110,27 +57,6 @@ const DeleteContentContainer = styled(FlexRow)`
     span {
         margin-left: 5px;
     }
-`;
-const FilterContentContainer = styled(FlexRow)`
-    flex-basis: 30%;
-    margin: 10px 0 10px 0;
-    justify-content: left;
-
-    & .userName {
-        margin-left: 10px;
-    }
-`;
-
-const FilterTitle = styled(FlexRow)`
-    font-size: 15px;
-    margin-bottom: 15px;
-    svg {
-        width: 20px;
-    }
-`;
-const Filter = styled.div`
-    min-height: 100px;
-    margin: 35px 30px;
 `;
 
 const SidebarContainer = styled.div`
---
+++ FE/src/components/issues/OptionSidebar.jsx
@@ -0,0 +1,186 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import { IconPlus } from '../../assets/icons/IconPlus';
+import { CustomProfile } from '../../assets/CustomProfile';
+import { IconProgressBar } from '../../assets/icons/IconProgressBar';
+import { getProgressPercentage } from '../../utils/issueUtils';
+import { CustomLabelBadge } from '../../assets/CustomLabelBadge';
+import { FlexRow } from '../../styles/theme';
+import { useLabelsFilter, useMembersFilter, useMilestonesFilter } from '../../hooks/useFiltersData';
+import { useQueryClient } from '@tanstack/react-query';
+
+const initActivePopup = {
+    assignee: false,
+    label: false,
+    milestone: false,
+};
+
+// export default function OptionSidebar({ milestone, assignees, labels }) {
+export default function OptionSidebar({ filterName, filterData, children }) {
+    const openIssueCount = filterData?.openIssueCount ?? 0;
+    const closedIssueCount = filterData?.closedIssueCount ?? 0;
+    const popupRef = useRef(null);
+
+    const [isActivePopup, setIsActivePopup] = useState(initActivePopup);
+    const [visiblePopupType, setVisiblePopupType] = useState('');
+    const [isAssigneeFetchPossible, setIsAssigneeFetchPossible] = useState(false);
+    const [isLabelFetchPossible, setIsLabelFetchPossible] = useState(false);
+    const [isMilestoneFetchPossible, setIsMilestoneFetchPossible] = useState(false);
+
+    const { data: assigneesData, isLoading: assiDataIsLoading } = useMembersFilter({
+        enabled: isAssigneeFetchPossible,
+    });
+    const { data: labelsData, isLoading: labelsDataIsLoading } = useLabelsFilter({
+        enabled: isLabelFetchPossible,
+    });
+    const { data: milestonesData, isLoading: milestonesDataIsLoading } = useMilestonesFilter({
+        enabled: isMilestoneFetchPossible,
+    });
+
+    const setActivePopup = (type, toggleState) => {
+        setIsActivePopup({ ...initActivePopup, [type]: toggleState });
+    };
+
+    const toggleDropDown = (type) => {
+        const toggleState = !isActivePopup[type];
+        setActivePopup(type, toggleState);
+    };
+
+    const toggleEnableByType = {
+        assignee: () => setIsAssigneeFetchPossible(true), //담당자
+        label: () => setIsLabelFetchPossible(true),
+        milestone: () => setIsMilestoneFetchPossible(true),
+    };
+
+    const handleMouseEnter = (type) => {
+        setIsActivePopup(true);
+        toggleEnableByType[type]();
+    };
+
+    useEffect(() => {
+        const activeType = Object.entries(isActivePopup);
+        if (activeType.every(([_, value]) => value === false)) setVisiblePopupType('');
+        else setVisiblePopupType(activeType.filter(([_, value]) => value === true)[0][0]);
+    }, [isActivePopup]);
+
+    return (
+        <>
+            <Filter>
+                <FilterTitle
+                    onClick={() => toggleDropDown(filterName)}
+                    onMouseEnter={() => {
+                        handleMouseEnter(filterName);
+                    }}
+                >
+                    <div>{children}</div>
+                    <IconPlus />
+                </FilterTitle>
+                {/* {filterData.map(({ id, imgUrl }) => (
+                    <FilterContentContainer key={id}>
+                        <CustomProfile src={imgUrl} alt={'assineeProfile'} />
+                        <span className="userName">{id}</span>
+                    </FilterContentContainer>
+                ))} */}
+
+                {visiblePopupType === filterName && <PopupContainer>팝업내용</PopupContainer>}
+            </Filter>
+            <StyledLine />
+
+            {/* <Filter>
+                <FilterTitle
+                    onClick={() => toggleDropDown('label')}
+                    onMouseEnter={() => {
+                        handleMouseEnter('label');
+                    }}
+                >
+                    <div>레이블</div>
+                    <IconPlus />
+                </FilterTitle>
+                <LabelContentContainer>
+                    {labels.map(({ id, name, description, textColor, bgColor }) => (
+                        <StyledLabel key={id} backgroundColor={bgColor} color={textColor}>
+                            {name}
+                        </StyledLabel>
+                    ))}
+                </LabelContentContainer>
+                {visiblePopupType === 'label' && <PopupContainer>ddd</PopupContainer>}
+            </Filter>
+            <StyledLine />
+            <Filter>
+                <FilterTitle
+                    onClick={() => toggleDropDown('milestone')}
+                    onMouseEnter={() => {
+                        handleMouseEnter('milestone');
+                    }}
+                >
+                    <div>마일스톤</div>
+                    <IconPlus />
+                </FilterTitle>
+                {milestone && (
+                    <>
+                        <FilterContentContainer>
+                            <IconProgressBar percentage={getProgressPercentage(openIssueCount, closedIssueCount)} />
+                        </FilterContentContainer>
+                        <FilterContentContainer>{milestone.name}</FilterContentContainer>
+                    </>
+                )}
+
+                {visiblePopupType === 'milestone' && <PopupContainer>ddd</PopupContainer>}
+            </Filter> */}
+        </>
+    );
+}
+
+const PopupContainer = styled.div`
+    position: absolute;
+    top: 10px;
+    right: 0;
+    min-width: 200px;
+    min-height: 200px;
+    border: 2px solid ${(props) => props.theme.borderColor};
+    border-radius: 20px;
+    background-color: antiquewhite;
+    z-index: 5;
+`;
+
+const StyledLabel = styled(CustomLabelBadge)`
+    /* height: 30px; */
+    margin-right: 10px;
+    font-size: 13px;
+`;
+
+const StyledLine = styled.div`
+    width: 100%;
+    height: 1px;
+    border-bottom: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const LabelContentContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin: 20px 0;
+`;
+
+const FilterContentContainer = styled(FlexRow)`
+    flex-basis: 30%;
+    margin: 10px 0 10px 0;
+    justify-content: left;
+
+    & .userName {
+        margin-left: 10px;
+    }
+`;
+
+const FilterTitle = styled(FlexRow)`
+    cursor: pointer;
+    font-size: 15px;
+    margin-bottom: 15px;
+    svg {
+        width: 20px;
+    }
+`;
+const Filter = styled.div`
+    position: relative;
+    min-height: 100px;
+    margin: 35px 30px;
+`;
