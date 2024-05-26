7a6a65e0b3b193d36e5c0033219564ddcb056260
zzawang <hobin0116@naver.com>
2024-05-26 18:37:48 +0900

    feat: 깃허브 로그인을 통해 사용자에게 유저 정보와 JWT 토큰을 전달하는 기능 구현
    - 처음 사용자가 깃허브 로그인을 하는 것이라면 회원가입 로직을 수행한다.
    - 사용자에게 유저 정보와 JWT 토큰을 전달한다.

--- be/issue-tracker/src/main/java/com/issuetracker/oauth/controller/GithubLoginController.java
+++ be/issue-tracker/src/main/java/com/issuetracker/oauth/controller/GithubLoginController.java
@@ -1,8 +1,14 @@
 package com.issuetracker.oauth.controller;
 
+import com.issuetracker.file.service.FileService;
+import com.issuetracker.file.util.ImgUrlConverter;
+import com.issuetracker.member.dto.LoginResponse;
+import com.issuetracker.member.service.LoginService;
+import com.issuetracker.member.service.MemberService;
+import com.issuetracker.member.util.MemberMapper;
 import com.issuetracker.oauth.dto.GithubProfile;
 import com.issuetracker.oauth.service.GithubLoginService;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class GithubLoginController {
     private final GithubLoginService githubLoginService;
+    private final MemberService memberService;
+    private final LoginService loginService;
+    private final FileService fileService;
 
     @GetMapping("/callback")
-    public ResponseEntity<GithubProfile> login(HttpServletResponse response, @RequestParam String code) {
+    public ResponseEntity<LoginResponse> login(@RequestParam String code) throws IOException {
         String accessToken = githubLoginService.getAccessToken(code);
         GithubProfile profile = githubLoginService.getUserInfo(accessToken);
 
-        return ResponseEntity.ok().body(profile);
+        // 처음 사용자가 깃허브 로그인을 하는 것이라면 회원가입 로직을 수행한다.
+        if (memberService.isNewUser(profile.getId())) {
+            memberService.create(MemberMapper.toMemberCreateDto(profile));
+            fileService.uploadFile(ImgUrlConverter.toMultipartFile(profile.getImgUrl()));
+        }
+        LoginResponse loginResponse = loginService.githubLogin(profile);
+        return ResponseEntity.ok().body(loginResponse);
     }
 }
