2b622d0d8eb8660897b949a9f41d5286d77886e6
zzawang <hobin0116@naver.com>
2024-05-26 18:27:40 +0900

    feat: 이미지 URL을 MultipartFile로 변환하는 기능 구현
    - 이미지 URL을 byte 배열로 변환한 후 변환된 배열로 MultipartFile 객체를 생성한다.

---
+++ be/issue-tracker/src/main/java/com/issuetracker/file/util/CustomMultipartFile.java
@@ -0,0 +1,63 @@
+package com.issuetracker.file.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * byte 배열을 MultipartFile 객체로 변환하기 위한 클래스
+ */
+public class CustomMultipartFile implements MultipartFile {
+    private byte[] input;
+    private String filename;
+
+    public CustomMultipartFile(byte[] input, String filename) {
+        this.input = input;
+        this.filename = filename;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return filename;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return input == null || input.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return input.length;
+    }
+
+    @Override
+    public byte[] getBytes() {
+        return input;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return new ByteArrayInputStream(input);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        try (FileOutputStream fos = new FileOutputStream(dest)) {
+            fos.write(input);
+        }
+    }
+}
