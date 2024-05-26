890fade90b8ac9a8929fce3336d1d1c56b6d3f8e
zzawang <hobin0116@naver.com>
2024-05-26 18:30:26 +0900

    feat: 깃허브 로그인 사용자를 MemberCreateDto로 매핑하는 기능 추가
    - 회원가입을 하기위해 필요한 MemberCreateDto로 매핑한다.

--- be/issue-tracker/src/main/java/com/issuetracker/member/util/MemberMapper.java
+++ be/issue-tracker/src/main/java/com/issuetracker/member/util/MemberMapper.java
@@ -3,12 +3,19 @@ package com.issuetracker.member.util;
 import com.issuetracker.member.dto.MemberCreateDto;
 import com.issuetracker.member.dto.SimpleMemberDto;
 import com.issuetracker.member.entity.Member;
+import com.issuetracker.oauth.dto.GithubProfile;
+import java.util.UUID;
 
 public class MemberMapper {
 
     public static Member toMember(MemberCreateDto memberCreateDto) {
         return new Member(memberCreateDto.getId(), memberCreateDto.getPassword(),
                 memberCreateDto.getNickname(), memberCreateDto.getEmail(), null);
+    }
+
+    public static MemberCreateDto toMemberCreateDto(GithubProfile profile) {
+        String uuidPw = UUID.randomUUID().toString().substring(0, 12); // 깃허브로 가입한 사용자는 비밀번호를 UUID로 설정
+        return new MemberCreateDto(profile.getId(), uuidPw, profile.getNickname(), profile.getEmail());
     }
 
     public static SimpleMemberDto toSimpleMemberDto(Member member, String imgUrl) {
