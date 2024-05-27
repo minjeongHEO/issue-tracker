e72b94e8716dc8ca3504de0d9921cc636e7246a9
minjeongHEO <hmjoung9770@gmail.com>
2024-05-27 18:32:10 +0900

    feat: userImg 함수 추가

--- FE/src/utils/userUtils.js
+++ FE/src/utils/userUtils.js
@@ -1,3 +1,6 @@
 export const getUserId = () => {
-    return localStorage.getItem('storeUserData') ? JSON.parse(localStorage.getItem('storeUserData')).id : '';
+    return localStorage.getItem('storeUserData') ? JSON.parse(localStorage.getItem('storeUserData')).memberProfile.id : '';
+};
+export const getUserImg = () => {
+    return localStorage.getItem('storeUserData') ? JSON.parse(localStorage.getItem('storeUserData')).memberProfile.imgUrl : '';
 };
