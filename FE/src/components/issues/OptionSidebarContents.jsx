416f7ccd63fa4b8270084ac5b47b0ad11dc561da
minjeongHEO <hmjoung9770@gmail.com>
2024-05-27 20:49:17 +0900

    feat: 이슈 상세 - 레이블, 마일스톤, 담당자 수정 적용

--- FE/src/components/issues/OptionSidebarContents.jsx
+++ FE/src/components/issues/OptionSidebarContents.jsx
@@ -3,9 +3,66 @@ import styled from 'styled-components';
 import { CustomProfile } from '../../assets/CustomProfile';
 import { CustomLabelBadge } from '../../assets/CustomLabelBadge';
 import { FlexRow } from '../../styles/theme';
-import { Checkbox } from 'antd';
+import { Checkbox, Radio } from 'antd';
+import { useModifyIssueAssignees, useModifyIssueLabels, useModifyIssueMilestone } from '../../hooks/useIssueDetailData';
 
-export default function OptionSidebarContents({ contents, filterName }) {
+export default function OptionSidebarContents({ contents, filterName, filterData, checkedDatas, setCheckedDatas, issueId }) {
+    const { mutate: modifyIssueLabels } = useModifyIssueLabels(String(issueId)); //labelIds
+    const { mutate: modifyIssueAssignees } = useModifyIssueAssignees(String(issueId)); //assigneeIds
+    const { mutate: modifyIssueMilestone } = useModifyIssueMilestone(String(issueId)); //milestoneId
+
+    const handleCheckBox = (event) => {
+        const id = event.currentTarget.dataset.id ?? '';
+
+        if (filterName === 'milestone') {
+            setCheckedDatas((prev) => {
+                return { ...prev, [filterName]: id };
+            });
+        } else {
+            setCheckedDatas((prev) => {
+                if (!prev) return;
+                const prevCheckedIds = prev[filterName] || [];
+                const targetIdx = prevCheckedIds.indexOf(id);
+                if (targetIdx === -1) return { ...prev, [filterName]: [...prevCheckedIds, id] };
+                else return { ...prev, [filterName]: prevCheckedIds.filter((item) => item !== id) };
+            });
+        }
+    };
+
+    const isChecked = (checkedId) => {
+        if (filterName === 'milestone') return checkedDatas === checkedId;
+        else return checkedDatas?.includes(checkedId);
+    };
+
+    const isModifyed = (previousData, currentData) => {
+        if (filterName === 'milestone') {
+            return String(previousData.id) !== currentData;
+        } else {
+            const previousIds = new Set(filterData.map(({ id }) => id));
+            const currnetIds = new Set(currentData);
+
+            if (previousIds.size !== currnetIds.size) return true;
+
+            previousIds.forEach(({ id }) => {
+                if (!currnetIds.has(id)) return true;
+            });
+
+            return false;
+        }
+    };
+
+    const modifyActionByType = {
+        assignee: () => modifyIssueAssignees({ assigneeIds: checkedDatas }),
+        label: () => modifyIssueLabels({ labelIds: checkedDatas }),
+        milestone: () => modifyIssueMilestone({ milestoneId: checkedDatas }),
+    };
+
+    useEffect(() => {
+        return () => {
+            if (isModifyed(filterData, checkedDatas)) modifyActionByType[filterName]();
+        };
+    }, [checkedDatas]);
+
     return (
         <>
             {contents.map((content) => (
@@ -22,10 +79,15 @@ export default function OptionSidebarContents({ contents, filterName }) {
                                 {content.name}
                             </StyledLabel>
                         )}
-                        {filterName === 'milestone' && <span className="titleName">{content.name}</span>}
+                        {filterName === 'milestone' && (
+                            <span className="titleName">
+                                {content.name}
+                                {content.isClosed && <span className="isClosed">닫힘</span>}
+                            </span>
+                        )}
                     </FlexRow>
                     <FlexRow className="itemRadio">
-                        <Checkbox />
+                        <Checkbox onClick={handleCheckBox} data-id={String(content.id)} checked={isChecked(String(content.id))} />
                     </FlexRow>
                 </StyledList>
             ))}
@@ -44,6 +106,11 @@ const StyledList = styled.li`
     }
     & .titleName {
         margin-left: 10px;
+    }
+    & .isClosed {
+        margin-left: 10px;
+        font-size: 10px;
+        font-style: italic;
     }
 `;
 
