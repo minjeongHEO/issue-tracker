3117af4c4d95bdf2fa00c09e82fb4635981c3a58
minjeongHEO <hmjoung9770@gmail.com>
2024-05-29 14:15:47 +0900

    feat: 레이블 페이지 레이아웃

--- FE/src/components/labels/Labels.jsx
+++ FE/src/components/labels/Labels.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import Header from '../header/Header';
+import { IndexContainer, MainContainer } from '../../styles/theme';
+import styled from 'styled-components';
 
 export default function labels() {
-    return <div>라벨 생성 페이지 입니다.</div>;
+    return (
+        <IndexContainer>
+            <Header />
+            <MainContainer>
+                <TitleContainer> 버튼들</TitleContainer>
+                <ContentsContainer>라벨 리스트</ContentsContainer>
+            </MainContainer>
+        </IndexContainer>
+    );
 }
+const TitleContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    background-color: antiquewhite;
+`;
+const ContentsContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    background-color: azure;
+`;
