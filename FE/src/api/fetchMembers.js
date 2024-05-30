c9fe0d7ec420cec65f95af98af21ae9cd9cb9fc5
minjeongHEO <hmjoung9770@gmail.com>
2024-05-30 19:58:52 +0900

    WIP: 깃허브 로그인 중

--- FE/src/api/fetchMembers.js
+++ FE/src/api/fetchMembers.js
@@ -28,3 +28,32 @@ export const fetchLogin = async ({ id, password }) => {
         throw error;
     }
 };
+/**
+ * 깃허브 로그인
+ * @param {Object} - {id, password}
+ * @returns {Object} - { result:boolean, data:{email, id, nickname} };
+    - 성공: 200
+    - 데이터 바인딩 실패: 400
+    - 로그인 실패 : 401
+ */
+export const fetchGithubLogin = async () => {
+    try {
+        const CLIENT_ID = 'Ov23liTzJL66RbPZt3fg';
+        const REDIRECT_URI = `${import.meta.env.VITE_TEAM_CLIENT}/members/callback`;
+        const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+
+        // https://github.com/login/oauth/authorize?client_id=Ov23liTzJL66RbPZt3fg&redirect_uri=https://api.issue-tracker.site/api/oauth/github/callback
+        // https://github.com/login/oauth/authorize?client_id=Ov23liTzJL66RbPZt3fg&redirect_uri={server 주소}/api/oauth/github/callback
+
+        const response = await fetch(githubAuthUrl, {
+            method: 'GET',
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const result = await response.json();
+        return { result: true, data: result };
+    } catch (error) {
+        throw error;
+    }
+};
