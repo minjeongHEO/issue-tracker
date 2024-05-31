0f2ac9726bece8794a4d7a551afeac08dc554bfc
최호빈 <hobinchoi0116@gmail.com>
2024-05-31 13:07:06 +0900

    feat: timezone 설정 (#105)

--- be/issue-tracker/src/main/java/com/issuetracker/IssueTrackerApplication.java
+++ be/issue-tracker/src/main/java/com/issuetracker/IssueTrackerApplication.java
@@ -1,12 +1,13 @@
 package com.issuetracker;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IssueTrackerApplication {
-
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));  // 타임존 설정
         SpringApplication.run(IssueTrackerApplication.class, args);
     }
 
